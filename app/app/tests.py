29b6c6eb00747c4f4095294550a4b568cdd5451e
vinDelphini <betadelphini.py@gmail.com>
2024-10-19 17:37:22 +0530

    fix pep8

--- app/app/tests.py
+++ app/app/tests.py
@@ -7,14 +7,13 @@ from app import calc
 
 class CalcTests(SimpleTestCase):
     """Test the calc module."""
-    
+
     def test_add_numbers(self):
         """Test adding numbers together."""
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
-    
+
     def test_sub_numbers(self):
         """Test subtracting numbers."""
         res = calc.subtract(15, 10)
         self.assertEqual(res, 5)
-        
